1cc82ba27d842c1e55df0ef7539dd76a3861fb38
Soham <soham_patil1@persistent.com>
2022-09-08 20:41:05 +0530

    Add existing project files to Git

--- Music Database/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://soham:<password>@cluster0.lrkgw.mongodb.net/test',(err)=>
-{
-    if(err)
-    {
-        console.log(err);
-    }
-    console.log("database connected!")
-})
